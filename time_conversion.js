67c6636139bac7d47f254b4e771cb74946e86a82
Erik Kralj <erik@kralj.dev>
2020-11-15 19:29:03 +0000

    add: birthday_cake_candles.js & time_conversion.js

---
+++ time_conversion.js
@@ -0,0 +1,18 @@
+const assert = require("assert").strict;
+
+function solution(s) {
+    let hour = parseInt(s.substring(0, 2), 10);
+    if (hour === 12) {
+        hour = 0;
+    }
+    const isPM = s.substring(s.length - 2, s.length) === 'PM';
+    if (isPM) {
+        hour += 12;
+    }
+    const hourPrefix = hour < 10 ? "0" : "";
+    return hourPrefix + hour.toString() + s.substring(2, s.length - 2);
+}
+
+assert.strictEqual(solution('12:01:00PM'), '12:01:00');
+assert.strictEqual(solution('12:01:00AM'), '00:01:00');
+assert.strictEqual(solution('07:05:45PM'), '19:05:45')
